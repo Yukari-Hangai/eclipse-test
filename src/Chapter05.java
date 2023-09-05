05a3ac022349cae19287b99751ae67b0fd13a8b0
YukariHangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-09-05 14:23:34 +0900

    Chapter 5 変数を学ぼう

---
+++ src/Chapter05.java
@@ -0,0 +1,13 @@
+
+public class Chapter05 {
+   public static void main(String[] args) {
+	   String name = "Yukari"; // string型の変数の宣言＆代入
+	   System.out.println(name); // 出力
+	   
+	   name = "Hangai"; // 再代入
+	   System.out.println(name); // 出力
+	   
+	   final int lucky_number = 777; // int型の定数の宣言＆代入
+	   System.out.println(lucky_number); // 出力
+   }
+}
