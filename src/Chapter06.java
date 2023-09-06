ced9698c22b184f1459328b552a292aae80f01a2
YukariHangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-09-06 14:49:58 +0900

    Chapter 6 型の基本を学ぼう

---
+++ src/Chapter06.java
@@ -0,0 +1,19 @@
+
+public class Chapter06 {
+   public static void main(String[] args) {
+      long long1 =  10000000000L;
+      System.out.println(long1);
+      double double1 = 3.14;
+      System.out.println(double1);
+      boolean boolean1 = true;
+      System.out.println(boolean1);
+      Integer integer1 = 100;
+      System.out.println(integer1);
+      Float float1 = 1.4142F;
+      System.out.println(float1);
+      Character character1 = 'う';
+      System.out.println(character1);
+      int[] array1 = new int[] { 0, 10, 20, 30, 40 };
+      System.out.println(array1[3]);
+   }
+}
