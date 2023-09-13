9518a311812c285b8bf8f4cfe03807cdae208c1c
YukariHangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-09-13 15:33:24 +0900

    Chapter 11 クラスとメソッドを理解しよう　途中

--- src/ClassMethod01.java
+++ src/ClassMethod01.java
@@ -1,10 +1,10 @@
 
 public class ClassMethod01 {
    public static void main(String[] args) {
-	   Human01 yamada = new Human01();
-	   System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
+	   Human01 yamada = new Human01(); // 引数なしのコンストラクタを用いてHuman01クラスのインスタンスを生成
+	   System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。"); // コンストラクタHuman01の初期値が表示される
 	
-	   Human01 sato = new Human01("佐藤", 25);
-	   System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
+	   Human01 sato = new Human01("佐藤", 25); // 引数を2つ用いてHuman01のクラスのインスタンスを生成
+	   System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。"); // コンストラクタの引数に渡した値が表示される
    }
 }
